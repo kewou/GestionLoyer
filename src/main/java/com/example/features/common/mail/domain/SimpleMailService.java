0f9a584d7f7a4cd9bbe73ce6125f3d3cd18b721f
CNANTCHOUANG <cnantchouang-ext@nexity.fr>
2024-06-16 14:50:19 +0200

    resolve integration tests errors

--- src/main/java/com/example/features/common/mail/domain/SimpleMailService.java
+++ src/main/java/com/example/features/common/mail/domain/SimpleMailService.java
@@ -3,10 +3,12 @@ package com.example.features.common.mail.domain;
 import com.example.features.common.mail.application.MessageService;
 import com.example.features.common.mail.dto.MessageDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("!test")
 public class SimpleMailService implements MessageService {
 
     @Autowired
