be623fef51ab8bac86937ffe0a3b3e4b9232f296
noumia <kewou.noumia@gmail.com>
2024-10-02 21:15:49 +0200

    Correction bug algo transaction

--- change.sql
+++ change.sql
@@ -1,2 +1,3 @@
 ALTER TABLE nom_table RENAME COLUMN ancien_nom_colonne TO nouveau_nom_colonne;
 ALTER TABLE appart ALTER COLUMN nom TYPE VARCHAR USING nom::VARCHAR;
+ALTER TABLE appart DROP CONSTRAINT uk_f3hw800p6cg6f5tlof83nabja;
